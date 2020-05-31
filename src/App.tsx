23e9628dbaa3e25ca93e7c79b44dfde8b12d5aab
EnochQin <qyqdream@gmail.com>
2020-05-31 21:37:27 +0800

    add react-router & Nav Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,67 @@
 import React from "react";
+import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.main`
+  flex: 1;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/pocket">
+              <Pocket />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/tags" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <div>
+          <Nav />
+        </div>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Pocket() {
+  return <h2>记账页</h2>;
+}
+
+function NoMatch() {
+  return <div>Page Not Found</div>;
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import styled from "styled-components";
+
+const NavWrap = styled.nav`
+  >ul{
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    display:flex;
+    >li{
+      width: 33.333%;
+      text-align: center;
+      padding: 10px 0;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrap>
+      <ul>
+        <li>
+          <Link to="/tags">标签</Link>
+        </li>
+        <li>
+          <Link to="/pocket">记账</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计</Link>
+        </li>
+      </ul>
+    </NavWrap>
+  );
+};
+
+export default Nav;
