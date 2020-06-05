a6983452f33f1180341d53bbe5d4d89b2bd50702
EnochQin <qyqdream@gmail.com>
2020-06-05 23:37:49 +0800

    finished: Tags Page - TagsSection UI

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -2,46 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 import Layout from 'components/Layout';
 
-const TagsWrap = styled.section`
+const TagsSection = styled.section`
   background: #fff;
-  >ol{
-    display: flex;
-    >li{
-      margin: 5px;
+  padding: 12px 16px;
+  > ol{
+    margin-left: -12px;
+    > li{
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
     }
   }
+  > button{
+    background: none;
+    border: none;
+    border-bottom: 1px solid #333;
+    padding: 2px 4px;
+    color: #666;
+    margin-top: 8px;
+  }
 `;
-const TagButton = styled.button`
-  font-size: 14px;
-  border-radius: 18px;
-  padding: 5px 20px;
-  line-height: 22px;
-  color: #484848; 
-  border: none;
-  background: #D9D9D9;
-`;
-const AddTagButton = styled.button``;
 const Tags = () => {
   return (
     <Layout>
-      <TagsWrap>
+      <TagsSection>
         <ol>
-          <li>
-            <TagButton>衣</TagButton>
-          </li>
-          <li>
-            <TagButton>食</TagButton>
-          </li>
-          <li>
-            <TagButton>住</TagButton>
-          </li>
-          <li>
-            <TagButton>行</TagButton>
-          </li>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
         </ol>
-
-        <AddTagButton>新增标签</AddTagButton>
-      </TagsWrap>
+        <button>新增标签</button>
+      </TagsSection>
 
       <section>
         <label>
