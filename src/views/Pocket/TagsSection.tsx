f1d0dac98a4ce1426e77d3f09d784fff8e8514fc
EnochQin <qyqdream@gmail.com>
2020-06-06 22:38:49 +0800

    finished: Pocket Module Function - add and select tag

--- src/views/Pocket/TagsSection.tsx
+++ src/views/Pocket/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, { useState } from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #fff;
   padding: 12px 16px;
   flex-grow: 1;
@@ -17,6 +18,9 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected{
+        background: #f60;
+      }
     }
   }
   > button{
@@ -28,5 +32,46 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = () => {
+  const [tagsList, setTagsList] = useState<Array<string>>(['衣', '食', '住', '行']);
+  const [selectedTagsList, setSelectedTagsList] = useState<Array<string>>([]);
+
+  const isTagsHasCurrentTag = (currentTag: string | null, tags: Array<string>): boolean => {
+    if (currentTag === null) return false;
+    return tags.includes(currentTag);
+  };
+  const addNewTag = () => {
+    const newTagName = window.prompt('请输入新标签的名字');
+    if (newTagName === null || newTagName === '') return;
+    if (isTagsHasCurrentTag(newTagName, tagsList)) {
+      window.alert('该标签名已存在，请不要重复添加！');
+    } else {
+      setTagsList([...tagsList, newTagName]);
+    }
+  };
+
+  const onToggleSelectedTag = (selectedTag: string) => {
+    if (isTagsHasCurrentTag(selectedTag, selectedTagsList)) {
+      setSelectedTagsList(selectedTagsList.filter(tag => tag !== selectedTag));
+    } else {
+      setSelectedTagsList([...selectedTagsList, selectedTag]);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <ol>
+        {
+          tagsList.map(tag => (
+            <li key={tag} className={isTagsHasCurrentTag(tag, selectedTagsList) ? 'selected' : ''}
+                onClick={() => onToggleSelectedTag(tag)}>{tag}</li>
+          ))
+        }
+      </ol>
+      <button onClick={addNewTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export { TagsSection };
--- src/views/Pocket/index.tsx
+++ src/views/Pocket/index.tsx
@@ -14,16 +14,7 @@ const PocketLayout = styled(Layout)`
 const Pocket = () => {
   return (
     <PocketLayout>
-      <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
-
+      <TagsSection />
       <NotesSection>
         <label>
           <span>备注</span>
