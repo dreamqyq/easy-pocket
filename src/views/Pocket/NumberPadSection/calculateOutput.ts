dc6b13bef6e79b73e6e5dcd6c685e09ed4476b57
EnochQin <qyqdream@gmail.com>
2020-06-07 21:38:17 +0800

    optimize: Pocket Module - NumberPadSection modularization

---
+++ src/views/Pocket/NumberPadSection/calculateOutput.ts
@@ -0,0 +1,33 @@
+import { InputString } from '../pocket';
+
+const calculateOutput = (text: InputString, originOutput: string): string => {
+  switch (true) {
+    case (!isNaN(parseInt(text))):
+      if (originOutput === '0') {
+        return text;
+      } else {
+        return originOutput + text;
+      }
+    case text === '.':
+      if (originOutput.includes('.')) {
+        return originOutput;
+      } else {
+        return originOutput + text;
+      }
+    case text === '删除': {
+      const length = originOutput.length;
+      if (length > 1) {
+        return originOutput.slice(0, -1) || '';
+      } else if (length === 1) {
+        return '';
+      }
+      return '';
+    }
+    case text === '清空':
+      return '';
+    default:
+      return '';
+  }
+};
+
+export { calculateOutput };
---
+++ src/views/Pocket/NumberPadSection/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Wrapper } from './Wrapper';
+import { calculateOutput } from './calculateOutput';
+import { InputString } from '../pocket';
+
+const NumberPadSection: React.FC = () => {
+  const [output, _setOutput] = useState<string>('0');
+  const setOutput = (value: string) => {
+    const length = value.length;
+    if (length > 16) {
+      value = value.slice(0, 16);
+    } else if (length === 0) {
+      value = '0';
+    }
+    _setOutput(value);
+  };
+  const editOutput = (event: React.MouseEvent) => {
+    const text = (event.target as HTMLInputElement).textContent;
+    if (text === null) return;
+    if (text === 'OK') {
+      // TODO
+      console.log('ok');
+    } else {
+      setOutput(calculateOutput(text as InputString, output));
+    }
+  };
+  return (
+    <Wrapper>
+      <div className='output'>{output}</div>
+      <div className='pad clearfix' onClick={editOutput}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className='ok'>OK</button>
+        <button className='zero'>0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export { NumberPadSection };
