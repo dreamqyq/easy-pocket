03197ad346c4ead0b0702b6fe4518755038d88ac
EnochQin <qyqdream@gmail.com>
2020-06-10 21:45:03 +0800

    finished: Tags Module - index UI

--- src/views/Tags/index.tsx
+++ src/views/Tags/index.tsx
@@ -2,22 +2,64 @@ import React from 'react';
 import Layout from 'components/Layout';
 import { useTags } from 'useTags';
 import { Link } from 'react-router-dom';
+import Icon from 'components/Icon';
+import styled from 'styled-components';
+
+const TagList = styled.ol`
+  background: #fff;
+  > li{
+    border-bottom: 1px solid #d5d5d5;
+    padding: 12px 0;
+    width: 94%;
+    margin-left: 3%;
+    > a{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      line-height: 20px;
+      > span {
+        max-width: 94%;
+      }
+    }
+  }
+`
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: #fff;
+`
+
+const Center = styled.div`
+  margin-top: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Tags: React.FC = () => {
   const { tags } = useTags();
   return (
     <Layout>
-      <ol>
+      <TagList>
         {
           tags.map(tag => (
             <li key={tag}>
               <Link to={`/tags/${tag}`}>
-                {tag}
+                <span className="oneLine">
+                  {tag}
+                </span>
+                <Icon name="right" />
               </Link>
             </li>
           ))
         }
-      </ol>
+      </TagList>
+      <Center>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 };
