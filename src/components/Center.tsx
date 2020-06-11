b4ec0f26d7612dbfc395aee044031644716bfe56
EnochQin <qyqdream@gmail.com>
2020-06-11 22:50:14 +0800

    add Button and Center components

---
+++ src/components/Center.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Center = styled.div`
+  margin-top: 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export { Center };
