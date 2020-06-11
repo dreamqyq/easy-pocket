bb3bb4030fc17e62b7d850f669d7bb1df9094065
EnochQin <qyqdream@gmail.com>
2020-06-11 23:02:25 +0800

    update: TagDetail UI WIP (Work In Process)

--- src/views/Tags/TagDetail.tsx
+++ src/views/Tags/TagDetail.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useTags } from 'hooks/useTags';
-import Layout from 'components/Layout';
+import { Layout } from 'components/Layout';
+import { Icon } from 'components/Icon';
+import { Button } from 'components/Button';
+import { Center } from 'components/Center';
+import styled from 'styled-components';
+
+const TopBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: #fff;
+`
 
 type Params = {
   tagId: string
@@ -12,8 +25,22 @@ const TagDetail: React.FC = () => {
   const tagName = findTagNameById(parseInt(tagId)).name
   return (
     <Layout>
+      <TopBar>
+        <Icon name="left" />
+        <span>编辑标签</span>
+        <Icon />
+      </TopBar>
+      <div>
+        <label>
+          <span>标签名</span>
+          <input type="text" placeholder="请输入标签名" />
+        </label>
         {tagName}
-    </Layout>  
+        <Center>
+          <Button>删除标签</Button>
+        </Center>
+      </div>
+    </Layout>
   )
 };
 
