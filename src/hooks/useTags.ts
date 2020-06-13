9acad22f76e28fd3b0be2ad7c533275bf73cc5fc
EnochQin <qyqdream@gmail.com>
2020-06-13 17:05:59 +0800

    optimize: useTags - updateTag and deleteTag

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -14,29 +14,13 @@ const useTags = () => {
   const findTagNameById = (id: number): Tag => {
     return tags.filter(tag => tag.id === id)[0];
   };
-  const findTagIndexById = (id: number): number => {
-    let index = -1;
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-    return index;
-  };
   const updateTag = (newTag: Tag): void => {
-    const index = findTagIndexById(newTag.id);
-    const tagsClone: Array<Tag> = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1, newTag);
-    setTags(tagsClone);
+    setTags(tags.map(tag => tag.id === newTag.id ? newTag : tag));
   };
   const deleteTag = (id: number): void => {
-    const index = findTagIndexById(id);
-    const tagsClone: Array<Tag> = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
-  return { tags, setTags, findTagNameById, findTagIndexById, updateTag, deleteTag };
+  return { tags, setTags, findTagNameById, updateTag, deleteTag };
 };
 
 export { useTags };
