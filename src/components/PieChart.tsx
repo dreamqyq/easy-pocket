1635b9a8f93391207ef69ab1de770d93f89fb45c
EnochQin <qyqdream@gmail.com>
2020-07-01 23:13:05 +0800

    add PieChart Component and edit Chart Page code(WIP)

---
+++ src/components/PieChart.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import { EChartOption, ECharts, init } from 'echarts';
+
+type Props = {
+  options: EChartOption
+}
+const PieChart: React.FC<Props> = (props) => {
+  const { options } = props;
+  const container = useRef({} as HTMLDivElement);
+  const chart = useRef({} as ECharts);
+
+  useEffect(() => {
+    const width = document.documentElement.clientWidth;
+    container.current.style.width = `${width - 40}px`;
+    container.current.style.height = `${(width - 40) * 1.2}px`;
+    chart.current = init(container.current, 'light');
+  }, []);
+
+  useEffect(() => {
+    console.log(options, chart.current);
+    chart.current.setOption(options);
+  }, [options]);
+
+  return (
+    <div ref={container} />
+  );
+};
+
+export { PieChart };
