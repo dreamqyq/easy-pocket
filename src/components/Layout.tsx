1c89c7d39639af5af650feb4a48d5437ac486ba7
EnochQin <qyqdream@gmail.com>
2020-07-13 11:46:47 +0800

    set default tags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, ReactNode } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Nav } from './Nav';
 
--- src/data/defaultTags.ts
+++ src/data/defaultTags.ts
@@ -1,11 +1,10 @@
 import { Tag } from 'types';
-import { createId } from 'utils/createId';
 
 const defaultTags: Array<Tag> = [
-  { id: createId(), name: '衣' },
-  { id: createId(), name: '食' },
-  { id: createId(), name: '住' },
-  { id: createId(), name: '行' }
+  { id: 1, name: '衣' },
+  { id: 2, name: '食' },
+  { id: 3, name: '住' },
+  { id: 4, name: '行' }
 ];
 
 export { defaultTags };
