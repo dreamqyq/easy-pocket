045cda71898f4641dfe00dd8e56d2f8f18ba8576
EnochQin <qyqdream@gmail.com>
2020-08-11 22:00:09 +0800

    fixed: Popup move active classname

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -5,27 +5,22 @@ const Wrapper = styled.div`
   position: fixed;
   bottom: 0;
   width: 100vw;
-  
-  > div{
-    transition: all 0.3s;
-    opacity: 0;
-    transform: translateY(100%);
-    &.active{
-      transform: translateY(0);
-      opacity: 1;
-    }
+  transition: all 0.3s;
+  opacity: 0;
+  transform: translateY(100%);
+  &.active {
+    transform: translateY(0);
+    opacity: 1;
   }
 `;
 
 type Props = {
-  show: boolean
-}
-const Popup: React.FC<Props> = (props) => {
-  return <Wrapper>
-    <div className={props.show ? 'active' : ''}>
-      {props.children}
-    </div>
-  </Wrapper>;
+  show: boolean;
+};
+const Popup: React.FC<Props> = props => {
+  return (
+    <Wrapper className={props.show ? 'active' : ''}>{props.children}</Wrapper>
+  );
 };
 
 export { Popup };
