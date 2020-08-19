884ad7d404b2abcb8e65cd816e36a74362c58edb
EnochQin <qyqdream@gmail.com>
2020-08-19 16:22:04 +0800

    calculateOutput.ts add some function | expression display WIP

--- src/views/Pocket/NumberPadSection/calculateOutput.ts
+++ src/views/Pocket/NumberPadSection/calculateOutput.ts
@@ -1,21 +1,48 @@
 import { InputString } from 'types/pocket';
 import { characterHasPlusOrMinus } from 'utils';
 
+const stringNumber2Decimal = (stringNumber: string): string => {
+  return parseFloat(stringNumber).toFixed(2);
+};
+
 const formatNumber = (text: InputString, originOutput: string): string => {
-  if (originOutput === '0') {
-    return text;
+  if (originOutput === '0.00') {
+    return stringNumber2Decimal(text);
   } else {
     const result = originOutput + text;
     const dotIndex = result.indexOf('.');
     if (dotIndex >= 0) {
       return result.slice(0, dotIndex + 3);
     }
-    return result;
+    return stringNumber2Decimal(result);
+  }
+};
+
+const expressionSplitByLastCharacter = (expression: string): Array<string> => {
+  /**
+   * "1+0.23-0.45+0.67-0.8" ==>
+   * ["1+0.23-0.45+0.67", "-", "0.8"]
+   */
+  return expression.split(/([-|+](?=[^\-|+]+$))/g);
+};
+
+const formatExpression = (text: InputString, originExpression: string): string => {
+  if (originExpression === '0.00') {
+    return text;
+  } else {
+    const array = expressionSplitByLastCharacter(originExpression);
+    if (array.length === 1) {
+      return originExpression + text;
+    } else {
+      const lastNumber = array[array.length - 1];
+      array[array.length - 1] = formatNumber(text, lastNumber);
+      return array.join('');
+    }
   }
 };
 
 const calculateExpression = (expression: string): string => {
-  if (expression === '') return '0';
+  if (expression === '') return '0.00';
   const array = expression.split(/([+|-])/g);
   let result = parseFloat(array[0]);
   array.forEach((item, index) => {
@@ -29,26 +56,37 @@ const calculateExpression = (expression: string): string => {
   return result.toFixed(2);
 };
 
+const formatNumberWithDot = (text: string, output: string): string => {
+  return output.includes('.') ? output : output + text;
+};
+
 type OutPutObj = {
   expression: string;
   output: string
 }
 const calculateOutput = (text: InputString, originOutput: string, originExpression: string): OutPutObj => {
+  debugger
   const result: OutPutObj = { expression: originExpression, output: originOutput };
   const length = originOutput.length;
   switch (true) {
     case !isNaN(parseInt(text)):
       result.output = formatNumber(text, originOutput);
+      result.expression = formatExpression(text, originExpression);
       break;
     case text === '.':
-      result.output = originOutput.includes('.') ? originOutput : originOutput + text;
+      result.output = formatNumberWithDot(text, originOutput);
+      const array = expressionSplitByLastCharacter(originExpression);
+      array[array.length - 1] = formatNumberWithDot(text, array[array.length - 1]);
+      result.expression = array.join('');
       break;
     case characterHasPlusOrMinus(text):
-      const originLastWord = originOutput.slice(-1);
+      const originLastWord = originExpression.slice(-1);
       result.expression = characterHasPlusOrMinus(originLastWord) ? originOutput : originOutput + text;
+      result.output = calculateExpression(result.expression);
       break;
     case text === '删除' && length > 1:
       result.output = originOutput.slice(0, -1) || '';
+      result.expression = originExpression.slice(0, -1) || '';
       break;
     case text === '清空':
     default:
