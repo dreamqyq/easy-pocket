d26091fe1ebebe0f0a9b3786c1a52800485d5f10
EnochQin <qyqdream@gmail.com>
2020-08-25 11:16:25 +0800

    fix: NoMatch page style

--- src/views/NoMatch.tsx
+++ src/views/NoMatch.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  text-align: center;
+  padding: 30px;
+  > p {
+    margin: 16px;  
+  }
+  a {
+    color: blue;
+    text-decoration: underline;
+  }
+`;
 
 const NoMatch: React.FC = () => {
-  return <div>Page is Not Found</div>;
+  return <Wrapper>
+    <p> 404 </p>
+    <p> Page is Not Found </p>
+    <Link to="/">Visit homepage -></Link>
+  </Wrapper>;
 };
 
 export default NoMatch;
